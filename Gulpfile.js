ac9f8723a6db4a1a21c23c93cca0053d9c04b32c
Atticus White <contact@atticuswhite.com>
2015-10-20 23:14:23 -0400

    gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     concat = require('gulp-concat'),
+    htmlmin = require('gulp-htmlmin'),
+    path = require('path'),
     paths = {
+      site: './_site',
       dist: './dist'
     };
 
@@ -11,4 +14,18 @@ gulp.task('less', function () {
   .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', ['less']);
+gulp.task('jekyll', function (done) {
+  var spawn = require('child_process').spawn,
+      jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+  jekyll.on('exit', function (code) {
+    done(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
+  });
+});
+
+gulp.task('html', ['jekyll'], function () {
+  gulp.src(path.join(paths.site, '**/*.html'))
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest(paths.site));
+});
+
+gulp.task('build', ['less', 'html']);
