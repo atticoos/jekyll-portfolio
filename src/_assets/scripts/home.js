dad9c25bd7b090c2cdc0159e995d62d864510175
Atticus White <contact@atticuswhite.com>
2015-10-24 16:47:50 -0400

    scrolling

--- src/_assets/scripts/home.js
+++ src/_assets/scripts/home.js
@@ -13,10 +13,11 @@
     });
     $('.site-nav nav a[href*=#]').click(function () {
       var targetSelector = $(this).attr('href').replace('/', ''),
+          headerHeight = $('.site-header').height(),
           position = 0;
 
       if (targetSelector !== '#') {
-        position = $(targetSelector).offset().top;
+        position = $(targetSelector).offset().top - headerHeight - 14;
       }
       $('html,body').animate({
         scrollTop: position + 'px'
