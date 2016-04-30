f445524328b1ea57502461f2093ec724e786b0d5
Atticus White <contact@atticuswhite.com>
2016-04-30 03:09:15 -0400

    deployment notifications during shipit hooks

--- shipitfile.js
+++ shipitfile.js
@@ -1,7 +1,9 @@
 var path = require('path');
+var fetch = require('node-fetch');
 
 module.exports = function (shipit) {
   require('shipit-deploy')(shipit);
+  var deploymentId = null;
 
   shipit.initConfig({
     default: {
@@ -30,4 +32,58 @@ module.exports = function (shipit) {
     var builtDirectory = path.resolve('./public/');
     shipit.remoteCopy(builtDirectory, shipit.releasePath);
   });
+
+  // notify github deployment has started
+  shipit.on('deploy', function (callback) {
+    var endpoint = [
+      'repos',
+      process.env.CIRCLE_PROJECT_USERNAME,
+      process.env.CIRCLE_PROJECT_REPONAME,
+      'deployments'
+    ].join('/');
+    var payload = {
+      ref: process.env.CIRCLE_BRANCH,
+      take: 'deploy',
+      environment: 'staging',
+      description: 'Deployment for ' + process.env.CIRCLE_BRANCH,
+      production_environemnt: false,
+      auto_merge: false
+    };
+    makeGithubRequest(endpoint, payload).then(function (deployment) {
+      console.log('github deployment created', deployment);
+      deploymentId = deployment.id;
+    })
+  });
+
+  // notify github deployment has completed
+  shipit.on('deployed', function (callback) {
+    var endpoint = [
+      'repos',
+      process.env.CIRCLE_PROJECT_USERNAME,
+      process.env.CIRCLE_PROJECT_REPONAME,
+      'deployments',
+      deploymentId,
+      'statuses'
+    ].join('/');
+    var payload = {
+      state: 'success',
+      environment_url: 'http://pr' + process.env.CIRCLE_BUILD_NUM + '.provision.atticuswhite.com/'
+    };
+    makeGithubRequest(endpoint, payload);
+  });
 };
+
+function makeGithubRequest(endpoint, body) {
+  var url = 'https://api.github.com/' + endpoint + '?access_token=' + process.env.GITHUB_DEPLOYMENT_TOKEN;
+  console.log('Making github request', url, body);
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }).then(function (response) {
+    return response.json();
+  });
+}
