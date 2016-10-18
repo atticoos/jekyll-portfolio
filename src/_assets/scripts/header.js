839c9502e321fd1b3eb53313e94d9987f395cda9
Atticus White <contact@atticuswhite.com>
2016-10-18 18:18:07 -0400

    asset updates

--- src/_assets/scripts/header.js
+++ src/_assets/scripts/header.js
@@ -1,20 +1,25 @@
 (function ($) {
   $(document).ready(function () {
-    $('.site-header .amburgah').click(function () {
-      $('.site-header').toggleClass('active');
-      return false;
-    });
+    // $('.site-header .amburgah').click(function () {
+    //   $('.site-header').toggleClass('active');
+    //   return false;
+    // });
+    //
+    // $('.landing .amburgah').click(function () {
+    //   $('.landing').toggleClass('ham');
+    //   return false;
+    // });
+    //
+    // $('.landing .ham-overlay .portfolio').click(function () {
+    //   $('html,body').animate({
+    //     scrollTop: $('#portfolio').offset().top
+    //   });
+    //   $('.landing').removeClass('ham')
+    // });
 
-    $('.landing .amburgah').click(function () {
-      $('.landing').toggleClass('ham');
+    $('.amburgah').click(function () {
+      $('body').toggleClass('ham');
       return false;
-    });
-
-    $('.landing .ham-overlay .portfolio').click(function () {
-      $('html,body').animate({
-        scrollTop: $('#portfolio').offset().top
-      });
-      $('.landing').removeClass('ham')
     });
   });
 }).call(this, jQuery);
